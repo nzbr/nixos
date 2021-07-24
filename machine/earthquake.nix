6f62aa0b39dda67f6fc06eb72340b13c51ce45d7
nzbr <mail@nzbr.de>
2021-07-24 07:55:12 +0200

    add modprobe config for macos qemu guests to earthquake.nix

--- machine/earthquake.nix
+++ machine/earthquake.nix
@@ -392,4 +392,11 @@
       }
     ];
   };
+
+  # Modprobe config for macOS VM
+  boot.extraModprobeConfig = ''
+    options kvm_intel nested=1
+    options kvm_intel emulate_invalid_guest_state=0
+    options kvm ignore_msrs=1 report_ignored_msrs=0
+  '';
 }
