4cd85e3d2dd18b399d845133db756515bd1cafc7
nzbr <mail@nzbr.de>
2021-08-12 16:30:24 +0200

    working live dvd config

--- machine/hurricane.nix
+++ machine/hurricane.nix
@@ -11,4 +11,5 @@
     ../module/desktop/latex.nix
     ../module/desktop/development.nix
   ];
+
 }
--- machine/live.nix
+++ machine/live.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, inputs, pkgs, modulesPath, ... }:
 {
   networking.hostName = "live";
 
@@ -6,25 +6,38 @@
     "${modulesPath}/installer/cd-dvd/installation-cd-base.nix"
     "${modulesPath}/installer/cd-dvd/channel.nix"
 
-    ../module/desktop.nix
+    ../module/common.nix
+    # desktop.nix contents are provided within this file
     ../module/desktop/gnome.nix
+    ../module/desktop/theme
+    ../module/desktop/pulseaudio.nix
+    ../module/desktop/device/razer-nari.nix
   ];
 
-  isoImage = {
-    edition = "nzbr";
-  };
 
-  users.users.nzbr.uid = 1000;
-  services.getty.autologinUser = lib.mkForce "nzbr";
+  environment.systemPackages = with pkgs; [
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
+    # partitioning and recovery tools
+    ddrescue
+    fsarchiver
+    gparted
+    partimage
+    testdisk
+    testdisk-qt
 
+    # Needed for the dotfiles
+    antibody
 
+    # Desktop packages
+    unstable.vivaldi
+    unstable.vivaldi-ffmpeg-codecs
+    vlc
+    xsel
+    lm_sensors
+    gnome.gnome-tweak-tool
+  ];
 
-  ### FROM https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/installer/cd-dvd/installation-cd-graphical-base.nix ###
-
-  networking.networkmanager.enable = true;
-  networking.wireless.enable = lib.mkForce false;
+  ### GRAPHICAL BASE ###
 
   # Whitelist wheel users to do anything
   # This is useful for things like pkexec
@@ -40,5 +53,105 @@
     });
   '';
 
-  services.xserver.displayManager.gdm.autoSuspend = false;
+  # KDE complains if power management is disabled (to be precise, if
+  # there is no power management backend such as upower).
+  powerManagement.enable = true;
+
+
+  ### CUSTOM ###
+
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    roboto
+    roboto-slab
+    roboto-mono
+  ];
+
+  isoImage = {
+    edition = "nzbr";
+    isoName = with config.system.nixos; with config.isoImage; lib.mkForce "${isoBaseName}-${edition}-${release}-${codeName}-${pkgs.stdenv.hostPlatform.system}.iso";
+  };
+
+  users.users.nzbr = {
+    uid = 1000;
+    extraGroups = [ "networkmanager" ];
+  };
+  services.getty.autologinUser = lib.mkForce "nzbr";
+
+  services = {
+    xserver = {
+      enable = true;
+      libinput.enable = true;
+
+      ### GNOME ###
+
+      desktopManager.gnome.favoriteAppsOverride = lib.mkForce ''
+        [org.gnome.shell]
+        favorite-apps=[ 'vivaldi-stable.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.DiskUtility.desktop', 'gparted.desktop' ]
+      '';
+
+      displayManager = {
+        gdm = {
+          wayland = false;
+          # autoSuspend makes the machine automatically suspend after inactivity.
+          # It's possible someone could/try to ssh'd into the machine and obviously
+          # have issues because it's inactive.
+          # See:
+          # * https://github.com/NixOS/nixpkgs/pull/63790
+          # * https://gitlab.gnome.org/GNOME/gnome-control-center/issues/22
+          autoSuspend = false;
+        };
+      };
+    };
+
+    flatpak.enable = true;
+    thermald.enable = true;
+    power-profiles-daemon.enable = false;
+    tlp.enable = true;
+  };
+
+  xdg.portal.enable = true;
+
+  networking = {
+    wireless = {
+      enable = lib.mkForce false; # Overwrite iso base
+      iwd.enable = true;
+    };
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+      packages = with pkgs; [
+        networkmanager-openvpn
+        networkmanager-openconnect
+      ];
+    };
+  };
+
+  system.activationScripts.copy-dotfiles = {
+    deps = [ "etc" ];
+    text =
+      let
+        script = pkgs.writeScript "dotfiles.sh" ''
+          echo "Setting up dotfiles for $(whoami)..."
+          rsync -ar "${inputs.dotfiles}/." "$HOME/.dotfiles"
+          mkdir -p $HOME/{.cache,.config,.local/{bin,share,lib}}
+          touch $HOME/{.cache,.config,.local/{bin,share,lib}}/.stowkeep
+          export DOT_NOINSTALL=1 && source $HOME/.dotfiles/control.sh && autolink_all
+          rm -f $HOME/{.cache,.config,.local/{bin,share,lib}}/.stowkeep
+          # sha256sum $HOME/.zsh_plugins.txt $HOME/.zshrc > $HOME/.zsh.sha
+          mkdir -p "$HOME/.cache/antibody"
+          ln -s "${pkgs.antibody}/bin/antibody" "$HOME/.cache/antibody/antibody"
+        '';
+      in
+      ''
+        # Add all installed packages to path
+        for i in ${toString config.environment.systemPackages}; do
+            PATH=$PATH:$i/bin:$i/sbin
+        done
+        sudo -u nzbr bash "${script}"
+        HOME=/root bash "${script}"
+      '';
+  };
+
+  environment.etc."nixos/config".source = ./..;
 }
