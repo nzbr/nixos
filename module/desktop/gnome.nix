9cb014f67f1c83938d251a0caca194cc413dba3e
nzbr <mail@nzbr.de>
2021-08-12 16:31:38 +0200

    switch to gnome-shell 40, install extensions

--- module/desktop/gnome.nix
+++ module/desktop/gnome.nix
@@ -3,7 +3,10 @@
   services.xserver = {
     desktopManager.gnome = {
       enable = true;
-      # TODO: favoriteAppsOverride (see: live.nix)
+      favoriteAppsOverride = ''
+        [org.gnome.shell]
+        favorite-apps=[ 'vivalid-stable.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'code.desktop', 'idea-ultimate.desktop', 'gitkraken.desktop', 'insomnia.desktop', 'timeular.desktop', 'discord.desktop', 'spotify.desktop' ]
+      '';
     };
     displayManager = {
       gdm = {
@@ -18,32 +21,41 @@
   };
 
   environment.systemPackages = with pkgs; [
+    gnome.dconf-editor
     # gnome.gnome-boxes
     gnome.gnome-tweak-tool
     gnome.seahorse
 
     local.gnome-shell-extension-pop-shell
-  ] /* ++ (with pkgs.gnomeExtensions; [
-     caffeine
-     dash-to-dock
-     gsconnect
-   ])*/;
+  ] ++ (with pkgs.gnomeExtensions; [
+    audio-switcher-40
+    blur-my-shell
+    caffeine
+    dash-to-panel
+    gsconnect
+    system-action-hibernate
+    syncthing-icon
+    tray-icons-reloaded
+    tweaks-in-system-menu
+  ]);
 
   programs.gnupg.agent.pinentryFlavor = "gnome3";
 
   programs.dconf.enable = true;
   services.dbus.packages = with pkgs; [ gnome.dconf ];
 
+  services.udev.packages = with pkgs; [ gnome3.gnome-settings-daemon ];
+
   nixpkgs.overlays = [
     (self: super: {
       gnome = super.gnome.overrideScope' (self': super': {
-        gnome-shell = super.legacy.gnome3.gnome-shell;
-        gnome-terminal = super.legacy.gnome3.gnome-terminal.overrideAttrs (oldAttrs: rec {
+        # gnome-shell = super.legacy.gnome3.gnome-shell;
+        gnome-terminal = super.gnome.gnome-terminal.overrideAttrs (oldAttrs: rec {
           patches =
             let
               repo = builtins.fetchGit {
                 url = "https://aur.archlinux.org/gnome-terminal-transparency.git";
-                rev = "3a48ec49f7aec584d505d54bbb0325d8561021a2";
+                rev = "b319fb2fa68d7aaff8361cbbca79b23c4e2b29c9";
               };
               transparencyPatch = repo + "/transparency.patch";
             in
