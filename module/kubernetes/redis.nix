51f33cd1377cda0e9f6c365fd64ec2d6454996bd
nzbr <mail@nzbr.de>
2021-10-28 06:55:27 +0200

    redis deployment

---
+++ module/kubernetes/redis.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, inputs, ... }:
+with builtins; with lib; {
+  kubenix.deployment.redis = {
+    dependencies = [ "rook-ceph" ];
+    steps = [
+      {
+        chart = {
+          repository = "bitnami";
+          name = "redis";
+        };
+        name = "redis";
+        namespace = "redis";
+        values = config.nzbr.assets."k8s/redis-values.yaml";
+      }
+    ];
+  };
+}
