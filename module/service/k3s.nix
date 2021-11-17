8bee21c8f5fc45848af782dfb0e8f42e5c7c7c9a
nzbr <mail@nzbr.de>
2021-11-17 06:01:48 +0100

    fix dns resolution in containers

--- module/service/k3s.nix
+++ module/service/k3s.nix
@@ -3,15 +3,17 @@ with builtins; with lib; {
   options.nzbr.service.k3s = {
     enable = mkEnableOption "K3s";
     dbHost = mkStrOpt "storm.nzbr.github.beta.tailscale.net";
+    dns = mkStrOpt "100.100.100.100";
   };
 
   config =
   let
     cfg = config.nzbr.service.k3s;
+    resolvconf = pkgs.writeText "resolv.conf" ''
+      nameserver ${cfg.dns}
+    '';
   in
   mkIf cfg.enable {
-    # Was needed for longhorn, which I don't use anymore
-    # nzbr.service.openIscsi.enable = true;
 
     services.k3s = {
       enable = true;
@@ -21,22 +23,32 @@ with builtins; with lib; {
 
     systemd.services.k3s = {
       serviceConfig = {
+        after = [
+          "network.service"
+          "firewall.service"
+          "docker.service"
+          "tailscaled.service"
+        ];
         ExecStart = lib.mkForce (
-          "${pkgs.busybox}/bin/sh -c '${pkgs.k3s}/bin/k3s server"
+          "${pkgs.busybox}/bin/sh -c '"
+          + "export PATH=$PATH:${pkgs.fuse-overlayfs}/bin:${pkgs.fuse3}/bin" # PATH is set with ENVIRONMENT= and not Path=, so it can't be easily overwritten, irrelevant for docker
+          + "&& ${pkgs.k3s}/bin/k3s server"
           + " --disable=traefik"
           + " --docker"
           + " --cluster-cidr=10.12.0.0/16"
           + " --service-cidr=10.13.0.0/16"
           + " --cluster-dns=10.13.0.10"
           + " --cluster-domain=kube"
+          + " --advertise-address=${config.nzbr.nodeIp}"
           + " --node-ip=${config.nzbr.nodeIp}"
           + " --node-external-ip=${config.nzbr.nodeIp}"
           + " --flannel-iface=tailscale0"
           + " --flannel-backend=vxlan"
+          + " --resolv-conf=${resolvconf}"
           + " --datastore-endpoint=postgres://kubernetes:$(cat ${config.nzbr.assets."k3s-db.password"})@${cfg.dbHost}:5432/kubernetes?sslmode=disable"
           + " --disable-network-policy"
           + " --write-kubeconfig /run/kubeconfig"
-          + " --snapshotter=native"
+          + " --snapshotter=fuse-overlayfs" # irrelevant for docker
           + "'"
         );
       };
@@ -59,9 +71,12 @@ with builtins; with lib; {
       KUBECONFIG = "/run/kubeconfig";
     };
 
-    networking.firewall.trustedInterfaces = [ "tunl0" ];
+    networking.firewall.trustedInterfaces = [ "tunl0" "flannel.1" "cni0" ];
 
-    # Needed for Rook-CEPH
-    boot.kernelModules = [ "rbd" ];
+    boot.kernelModules = [
+      "rbd" # CEPH (RADOS block device)
+    ];
+
+    virtualisation.docker.autoPrune.enable = true;
   };
 }
