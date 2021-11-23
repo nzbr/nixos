d95530da5103eec1ccf195e4027f46c48d836c82
nzbr <mail@nzbr.de>
2021-11-23 11:17:01 +0100

    nixpkgs-fmt

--- module/service/k3s.nix
+++ module/service/k3s.nix
@@ -7,76 +7,76 @@ with builtins; with lib; {
   };
 
   config =
-  let
-    cfg = config.nzbr.service.k3s;
-    resolvconf = pkgs.writeText "resolv.conf" ''
-      nameserver ${cfg.dns}
-    '';
-  in
-  mkIf cfg.enable {
+    let
+      cfg = config.nzbr.service.k3s;
+      resolvconf = pkgs.writeText "resolv.conf" ''
+        nameserver ${cfg.dns}
+      '';
+    in
+    mkIf cfg.enable {
 
-    services.k3s = {
-      enable = true;
-      docker = true;
-      role = "server";
-    };
-
-    systemd.services.k3s = {
-      serviceConfig = {
-        after = [
-          "network.service"
-          "firewall.service"
-          "docker.service"
-          "tailscaled.service"
-        ];
-        ExecStart = lib.mkForce (
-          "${pkgs.busybox}/bin/sh -c '"
-          + "export PATH=$PATH:${pkgs.fuse-overlayfs}/bin:${pkgs.fuse3}/bin" # PATH is set with ENVIRONMENT= and not Path=, so it can't be easily overwritten, irrelevant for docker
-          + "&& ${pkgs.k3s}/bin/k3s server"
-          + " --disable=traefik"
-          + " --docker"
-          + " --cluster-cidr=10.12.0.0/16"
-          + " --service-cidr=10.13.0.0/16"
-          + " --cluster-dns=10.13.0.10"
-          + " --cluster-domain=kube"
-          + " --advertise-address=${config.nzbr.nodeIp}"
-          + " --node-ip=${config.nzbr.nodeIp}"
-          + " --node-external-ip=${config.nzbr.nodeIp}"
-          + " --flannel-iface=tailscale0"
-          + " --flannel-backend=vxlan"
-          + " --resolv-conf=${resolvconf}"
-          + " --datastore-endpoint=postgres://kubernetes:$(cat ${config.nzbr.assets."k3s-db.password"})@${cfg.dbHost}:5432/kubernetes?sslmode=disable"
-          + " --disable-network-policy"
-          + " --write-kubeconfig /run/kubeconfig"
-          + " --snapshotter=fuse-overlayfs" # irrelevant for docker
-          + "'"
-        );
+      services.k3s = {
+        enable = true;
+        docker = true;
+        role = "server";
       };
+
+      systemd.services.k3s = {
+        serviceConfig = {
+          after = [
+            "network.service"
+            "firewall.service"
+            "docker.service"
+            "tailscaled.service"
+          ];
+          ExecStart = lib.mkForce (
+            "${pkgs.busybox}/bin/sh -c '"
+            + "export PATH=$PATH:${pkgs.fuse-overlayfs}/bin:${pkgs.fuse3}/bin" # PATH is set with ENVIRONMENT= and not Path=, so it can't be easily overwritten, irrelevant for docker
+            + "&& ${pkgs.k3s}/bin/k3s server"
+            + " --disable=traefik"
+            + " --docker"
+            + " --cluster-cidr=10.12.0.0/16"
+            + " --service-cidr=10.13.0.0/16"
+            + " --cluster-dns=10.13.0.10"
+            + " --cluster-domain=kube"
+            + " --advertise-address=${config.nzbr.nodeIp}"
+            + " --node-ip=${config.nzbr.nodeIp}"
+            + " --node-external-ip=${config.nzbr.nodeIp}"
+            + " --flannel-iface=tailscale0"
+            + " --flannel-backend=vxlan"
+            + " --resolv-conf=${resolvconf}"
+            + " --datastore-endpoint=postgres://kubernetes:$(cat ${config.nzbr.assets."k3s-db.password"})@${cfg.dbHost}:5432/kubernetes?sslmode=disable"
+            + " --disable-network-policy"
+            + " --write-kubeconfig /run/kubeconfig"
+            + " --snapshotter=fuse-overlayfs" # irrelevant for docker
+            + "'"
+          );
+        };
+      };
+
+      environment.systemPackages = with pkgs; [
+        k3s
+        kubectl
+        kubernetes-helm
+        local.kubectl-kadalu
+        local.kubectl-killnamespace
+      ];
+
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+
+      environment.variables = {
+        KUBECONFIG = "/run/kubeconfig";
+      };
+
+      networking.firewall.trustedInterfaces = [ "tunl0" "flannel.1" "cni0" ];
+
+      boot.kernelModules = [
+        "rbd" # CEPH (RADOS block device)
+      ];
+
+      virtualisation.docker.autoPrune.enable = true;
     };
-
-    environment.systemPackages = with pkgs; [
-      k3s
-      kubectl
-      kubernetes-helm
-      local.kubectl-kadalu
-      local.kubectl-killnamespace
-    ];
-
-    networking.firewall.allowedTCPPorts = [
-      80
-      443
-    ];
-
-    environment.variables = {
-      KUBECONFIG = "/run/kubeconfig";
-    };
-
-    networking.firewall.trustedInterfaces = [ "tunl0" "flannel.1" "cni0" ];
-
-    boot.kernelModules = [
-      "rbd" # CEPH (RADOS block device)
-    ];
-
-    virtualisation.docker.autoPrune.enable = true;
-  };
 }
