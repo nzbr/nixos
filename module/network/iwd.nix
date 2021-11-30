bf0f05c288b0f5dc97728ea8aad95b1b3728f8d6
nzbr <mail@nzbr.de>
2021-11-30 21:25:15 +0100

    iwd: fix agenix path

--- module/network/iwd.nix
+++ module/network/iwd.nix
@@ -19,7 +19,7 @@ with builtins; with lib; {
         text = ''
           mkdir -p /run/iwd
           find /run/iwd/ -type f -delete
-          for net in /run/secrets/iwd/*; do
+          for net in ${config.age.secretsMountPoint}/*/iwd/*; do
             FILE="/run/iwd/$(head -n1 $net)"
             tail -n+2 $net > "$FILE"
           done
@@ -35,8 +35,8 @@ with builtins; with lib; {
         };
         restartTriggers =
           mapAttrsToList
-          (name: type: hashFile "sha256" "${config.nzbr.flake.assets}/iwd/${name}")
-          (readDir "${config.nzbr.flake.assets}/iwd/");
+            (name: type: hashFile "sha256" "${config.nzbr.flake.assets}/iwd/${name}")
+            (readDir "${config.nzbr.flake.assets}/iwd/");
       };
     };
 }
