8485e19cc09547f328c7de7c45b0e66c6a7f1be6
nzbr <mail@nzbr.de>
2021-12-15 20:01:04 +0100

    latex: switch to tectonic

--- module/program/latex.nix
+++ module/program/latex.nix
@@ -9,51 +9,7 @@ with builtins; with lib; {
       pandoc
       pandoc-plantuml-filter
 
-      (texlive.combine {
-        inherit (texlive)
-          scheme-medium
-
-          academicons
-          adjustbox
-          arydshln
-          bidi
-          bookmark
-          booktabs
-          caption
-          circuitikz
-          csquotes
-          dinbrief
-          fancyvrb
-          float
-          fontawesome5
-          footmisc
-          footnotebackref
-          fvextra
-          hyperref
-          listings
-          ly1
-          mathspec
-          mdframed
-          moderncv
-          multirow
-          needspace
-          parskip
-          pagecolor
-          polyglossia
-          selnolig
-          setspace
-          sourcesanspro
-          sourcecodepro
-          stmaryrd
-          titling
-          ulem
-          unicode-math
-          upquote
-          xcolor
-          xpatch
-          zref
-          ;
-      })
+      tectonic
     ];
   };
 }
