5e8a0d0ee60ca5038dc41852edc3f473d434b2b3
nzbr <mail@nzbr.de>
2022-01-02 20:56:13 +0100

    armv7 binary cache

--- flake.nix
+++ flake.nix
@@ -125,6 +125,15 @@
                                     });
                               };
 
+                              nix = {
+                                binaryCaches = [
+                                  "https://thefloweringash-armv7.cachix.org"
+                                ];
+                                binaryCachePublicKeys = [
+                                  "thefloweringash-armv7.cachix.org-1:v+5yzBD2odFKeXbmC+OPWVqx4WVoIVO6UXgnSAWFtso="
+                                ];
+                              };
+
                               systemd.tmpfiles.rules = [
                                 "d /nix/build 0777 root root"
                               ];
