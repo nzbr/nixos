61b8d6dd23ed51f2d58516e8afcd7cd965203f31
nzbr <mail@nzbr.de>
2022-04-20 16:06:55 +0200

    split ssh client config

---
+++ module/cli/ssh-hosts.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, lib, ... }:
+with builtins; with lib;
+let
+  root = config.nzbr.flake.root;
+in
+{
+  services.openssh.knownHosts = mkIf (root != null) (
+    lib.listToAttrs
+      (map
+        (hostname:
+          lib.nameValuePair
+            hostname
+            {
+              hostNames = filter (x: x != "") ([
+                hostname
+                "${hostname}.nzbr.de"
+                "${hostname}4.nzbr.de"
+                "${hostname}6.nzbr.de"
+                "${hostname}.nzbr.github.beta.tailscale.net"
+              ]);
+              publicKeyFile = "${root}/host-key/${hostname}/ssh_host_ed25519_key.pub";
+            }
+        )
+        (
+          lib.mapAttrsToList
+            (name: _: name)
+            (readDir "${root}/host-key")
+        )
+      )
+  );
+}
--- module/cli/ssh-keys.nix
+++ module/cli/ssh-keys.nix
@@ -1,53 +1,25 @@
 { config, lib, inputs, system, ... }:
-with builtins; with lib; {
+with builtins; with lib;
+let
+  root = config.nzbr.flake.root;
+in
+{
   config =
-    (
-      mkIf (config.nzbr.assets ? "ssh/id_ed25519") {
-        age.secrets."ssh/id_ed25519" = {
-          owner = config.nzbr.user;
-          mode = "0600";
-        };
+    mkIf (root != null && (hasAttr "ssh" (readDir root))) {
+      age.secrets."ssh/id_ed25519" = {
+        owner = config.nzbr.user;
+        mode = "0600";
+      };
 
-        environment.extraInit =
-          let
-            id = config.nzbr.assets."ssh/id_ed25519";
-            id_pub = config.nzbr.assets."ssh/id_ed25519.pub";
-          in
-          ''
-            mkdir -p ~/.ssh
-            install -m0600 ${id} ~/.ssh/id_ed25519
-            install -m0600 ${id_pub} ~/.ssh/id_ed25519.pub
-          '';
-      }
-    ) // (
-      mkIf (config.nzbr.flake.root != null) (
+      environment.extraInit =
         let
-          root = config.nzbr.flake.root;
+          id = config.nzbr.assets."ssh/id_ed25519";
+          id_pub = config.nzbr.assets."ssh/id_ed25519.pub";
         in
-        {
-          services.openssh.knownHosts = lib.listToAttrs
-            (map
-              (hostname:
-                lib.nameValuePair
-                  hostname
-                  {
-                    hostNames = filter (x: x != "") ([
-                      hostname
-                      "${hostname}.nzbr.de"
-                      "${hostname}4.nzbr.de"
-                      "${hostname}6.nzbr.de"
-                      "${hostname}.nzbr.github.beta.tailscale.net"
-                    ]);
-                    publicKeyFile = "${root}/host-key/${hostname}/ssh_host_ed25519_key.pub";
-                  }
-              )
-              (
-                lib.mapAttrsToList
-                  (name: _: name)
-                  (readDir "${root}/host-key")
-              )
-            );
-        }
-      )
-    );
+        ''
+          mkdir -p ~/.ssh
+          install -m0600 ${id} ~/.ssh/id_ed25519
+          install -m0600 ${id_pub} ~/.ssh/id_ed25519.pub
+        '';
+    };
 }
