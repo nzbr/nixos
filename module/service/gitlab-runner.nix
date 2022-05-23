1bb854b1980ddb4b323009e537b0a8be6e193187
nzbr <mail@nzbr.de>
2022-05-23 22:47:27 +0200

    add gitlab runner for devsaur

--- module/service/gitlab-runner.nix
+++ module/service/gitlab-runner.nix
@@ -11,6 +11,38 @@ with builtins; with lib; {
   config =
     let
       cfg = config.nzbr.service.gitlab-runner;
+      nix-setup = pkgs.writeScript "nix-setup" ''
+        mkdir -p -m 0755 /nix/var/log/nix/drvs
+        mkdir -p -m 0755 /nix/var/nix/gcroots
+        mkdir -p -m 0755 /nix/var/nix/profiles
+        mkdir -p -m 0755 /nix/var/nix/temproots
+        mkdir -p -m 0755 /nix/var/nix/userpool
+        mkdir -p -m 1777 /nix/var/nix/gcroots/per-user
+        mkdir -p -m 1777 /nix/var/nix/profiles/per-user
+        mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
+
+        . ${pkgs.nix}/etc/profile.d/nix.sh
+        export PATH="$PATH:/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin"
+
+        ${pkgs.nix}/bin/nix-env -i ${concatStringsSep " " (with pkgs; [ nix cacert ])}
+
+        ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
+        ${pkgs.nix}/bin/nix-channel --update nixpkgs
+
+        ln -s $HOME/.nix-defexpr /.nix-defexpr
+      '';
+      nix-env = {
+        ENV = "/etc/profile";
+        USER = "root";
+        NIX_REMOTE = "daemon";
+        NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
+        NIX_PATH = "/.nix-defexpr/channels";
+      };
+      nix-volumes = [
+        "/nix/store:/nix/store:ro"
+        "/nix/var/nix/db:/nix/var/nix/db:ro"
+        "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
+      ];
     in
     mkIf cfg.enable {
       systemd.services.gitlab-runner.restartIfChanged = true;
@@ -22,58 +54,45 @@ with builtins; with lib; {
           {
             nixos = {
               registrationConfigFile = config.nzbr.assets."git.nzbr.de-runner-registration.env";
-              tagList = [ "nix" ];
+              tagList = [ "docker" "linux" "nix" ] ++ cfg.extraTags;
               runUntagged = false;
               executor = "docker";
-              dockerImage = "alpine";
-              dockerVolumes = [
-                "/nix/store:/nix/store:ro"
-                "/nix/var/nix/db:/nix/var/nix/db:ro"
-                "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
-              ];
-              preBuildScript = pkgs.writeScript "nix-setup" ''
-                mkdir -p -m 0755 /nix/var/log/nix/drvs
-                mkdir -p -m 0755 /nix/var/nix/gcroots
-                mkdir -p -m 0755 /nix/var/nix/profiles
-                mkdir -p -m 0755 /nix/var/nix/temproots
-                mkdir -p -m 0755 /nix/var/nix/userpool
-                mkdir -p -m 1777 /nix/var/nix/gcroots/per-user
-                mkdir -p -m 1777 /nix/var/nix/profiles/per-user
-                mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
-
-                . ${pkgs.nix}/etc/profile.d/nix.sh
-
-                ${pkgs.nix}/bin/nix-env -i ${concatStringsSep " " (with pkgs; [ nix cacert ])}
-
-                ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
-                ${pkgs.nix}/bin/nix-channel --update nixpkgs
-
-                ln -s $HOME/.nix-defexpr /.nix-defexpr
-              '';
-              environmentVariables = {
-                ENV = "/etc/profile";
-                USER = "root";
-                NIX_REMOTE = "daemon";
-                PATH = "/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin";
-                NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
-                NIX_PATH = "/.nix-defexpr/channels";
-              };
+              dockerImage = "archlinux";
+              dockerPrivileged = true;
+              dockerVolumes = nix-volumes;
+              preBuildScript = nix-setup;
+              environmentVariables = nix-env;
             };
+
             docker = {
               registrationConfigFile = config.nzbr.assets."git.nzbr.de-runner-registration.env";
               tagList = [ "docker" "linux" ] ++ cfg.extraTags;
               runUntagged = true;
               executor = "docker";
               dockerImage = "archlinux";
+              dockerPrivileged = true;
             };
 
             gitlab-com = {
               registrationConfigFile = config.nzbr.assets."gitlab.com-runner-registration.env";
-              tagList = [ "docker" "linux" "nzbr" ];
+              tagList = [ "docker" "linux" ];
               runUntagged = true;
               executor = "docker";
               dockerImage = "archlinux";
+              dockerPrivileged = true;
             };
+
+            # devsaur = {
+            #   registrationConfigFile = config.nzbr.assets."devsaur-runner-registration.env";
+            #   registrationFlags = [
+            #     "--name nzbr-${config.networking.hostName}"
+            #   ];
+            #   tagList = [ "docker" "linux" "nzbr" ];
+            #   runUntagged = true;
+            #   executor = "docker";
+            #   dockerImage = "archlinux";
+            #   dockerPrivileged = true;
+            # };
           };
       };
     };
