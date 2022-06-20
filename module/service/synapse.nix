fb2f8e89143344a3e9cb0b47b1894b3a4183145a
nzbr <mail@nzbr.de>
2022-06-20 18:01:05 +0200

    port synapse module

--- module/service/synapse.nix
+++ module/service/synapse.nix
@@ -15,53 +15,62 @@ with builtins; with lib; {
 
       services.matrix-synapse = {
         enable = true;
-        server_name = "nzbr.de";
-        public_baseurl = "https://nzbr.de:8448/";
-        servers = {
-          "matrix.org" = {
-            "ed25519:auto" = "Noi6WqcDj0QmPxCNQqgezwTlBKrfqehY1u2FyWP9uYw";
-          };
-        };
         dataDir = "/storage/synapse";
-        enable_registration = false;
-        listeners = [
-          {
-            bind_address = "";
-            port = 28448;
-            resources = [
-              {
-                compress = true;
-                names = [
-                  "client"
-                ];
-              }
-              {
-                compress = false;
-                names = [
-                  "federation"
-                ];
-              }
-            ];
-            tls = false;
-            type = "http";
-            x_forwarded = true;
-          }
-        ];
-        database_type = "psycopg2";
-        database_user = "synapse";
-        database_args = {
-          host = "storm.nzbr.github.beta.tailscale.net";
-        };
         withJemalloc = true;
-        max_upload_size = "50M";
+        settings = {
+          public_baseurl = "https://nzbr.de:8448/";
+          enable_registration = false;
+          server_name = "nzbr.de";
+          max_upload_size = "50M";
+          database = {
+            name = "psycopg2";
+            user = "synapse";
+            args = {
+              host = "storm.nzbr.github.beta.tailscale.net";
+            };
+          };
+          trusted_key_servers = [
+            {
+              server_name = "matrix.org";
+              verify_keys = {
+                "ed25519:auto" = "Noi6WqcDj0QmPxCNQqgezwTlBKrfqehY1u2FyWP9uYw";
+              };
+            }
+          ];
+          listeners = [
+            {
+              bind_addresses = [
+                config.nzbr.nodeIp
+              ];
+              port = 28448;
+              resources = [
+                {
+                  compress = true;
+                  names = [
+                    "client"
+                  ];
+                }
+                {
+                  compress = false;
+                  names = [
+                    "federation"
+                  ];
+                }
+              ];
+              tls = false;
+              type = "http";
+              x_forwarded = true;
+            }
+          ];
+          app_service_config_files = [
+          ];
+        };
         extraConfigFiles = [
           config.nzbr.assets."synapse.yaml"
 
           (pkgs.writeText "config.yaml" (toJSON {
             suppress_key_server_warning = true;
           }))
-        ];
-        app_service_config_files = [
         ];
       };
 
