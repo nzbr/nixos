acc4dbb36ba316d87fc4e7dc69c2b3d0d1a887f8
nzbr <mail@nzbr.de>
2023-04-11 13:23:50 +0200

    remote-unlock: tailscale support; pushbullet notifications

--- module/boot/remote-unlock.nix
+++ module/boot/remote-unlock.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, pkgs, modulesPath, inputs, ... }:
 with builtins; with lib;
 let
   cfg = config.nzbr.boot.remoteUnlock;
@@ -18,6 +18,13 @@ in
 
   config = mkIf cfg.enable {
     boot.initrd = {
+      secrets = mkIf config.nzbr.service.tailscale.enable {
+        "/var/lib/tailscale/tailscaled.state" = "/var/lib/tailscale/tailscaled.state";
+      };
+      kernelModules = mkIf config.nzbr.service.tailscale.enable [ "tun" "nf_tables" "nft_compat" ];
+      extraFiles = {
+        "/etc/ssl/certs/ca-certificates.crt".source = pkgs.runCommand "ca-bundle.crt" { } "cp ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt $out";
+      };
       network = {
         enable = true;
         ssh = {
@@ -26,7 +33,6 @@ in
           authorizedKeys = config.users.users.root.openssh.authorizedKeys.keys;
           hostKeys = [
             "/etc/ssh/ssh_host_ed25519_key"
-            # (builtins.unsafeDiscardStringContext "${root}/host/${config.networking.hostName}/ssh/ssh_host_ed25519_key")
           ];
         };
         postCommands =
@@ -61,12 +67,25 @@ in
               );
           in
           ''
+            # Print console log when logging in via SSH
+            echo "dmesg" >> /root/.profile
+
             # Ask for luks password when logging in via SSH
             ${luks}
             ${zfs}
 
+            # Set hostname
+            hostname ${config.networking.hostName}
+
             # Setup network
             ${ipconfig}
+
+            ${optionalString (config.nzbr.service.tailscale.enable) ''
+              export PATH="$PATH":${pkgs.iptables}/bin
+              # Start tailscaled
+              ${config.services.tailscale.package}/bin/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/run/tailscale/tailscaled.sock --port=${toString config.services.tailscale.port} &
+              sleep 3s
+            ''}
 
             echo ""
             echo '######################'
@@ -75,8 +94,17 @@ in
             echo ""
             ip a
             echo ""
+
+            {
+              export PATH="$PATH":${pkgs.openssl}/bin
+              KEY=$(${pkgs.rage}/bin/rage -d -i /etc/ssh/ssh_host_ed25519_key ${inputs.self}/asset/pushbulletKey.age)
+              ${pkgs.curl}/bin/curl --silent -u "$KEY": -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "note", "title": "Remote Unlock", "body": "${config.networking.hostName} requests a password"}' >/dev/null
+            } || true
           '';
       };
+      postMountCommands = ''
+        pkill -x tailscaled || true
+      '';
     };
   };
 }
