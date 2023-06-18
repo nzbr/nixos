3ce506509821be9d676fd672ebe41688e3a80bc4
nzbr <mail@nzbr.de>
2023-06-18 12:25:54 +0200

    update home-manager

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-wsl = {
