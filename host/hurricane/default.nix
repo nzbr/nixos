13adc691a423fb5bc527391318da3cbca92c34aa
nzbr <mail@nzbr.de>
2023-12-10 03:56:48 +0100

    hurricane: setup as remote builder

--- host/hurricane/default.nix
+++ host/hurricane/default.nix
@@ -4,5 +4,19 @@ with builtins; with lib; {
     system = "x86_64-linux";
     patterns = [ "common" "wsl" "development" ];
     pattern.development.guiTools = true;
+
+    remoteNixBuild.enable = true;
   };
+
+  services.openssh = {
+    enable = true;
+    ports = [
+      2222
+    ];
+  };
+
+  fileSystems."/tmp".options = mkForce [ "size=32G" ];
+
+  system.stateVersion = "23.05";
+  nzbr.home.config.home.stateVersion = "23.05";
 }
