ec5ac4afa7bcd0f546153b98c26078292ab2d80d
nzbr <mail@nzbr.de>
2024-06-04 23:00:52 +0200

    raspberrypi: initrd support (WIP)

--- module/boot/raspberrypi.nix
+++ module/boot/raspberrypi.nix
@@ -1,9 +1,12 @@
 # based upon https://github.com/NixOS/nixpkgs/blob/nixos-21.11/nixos/modules/system/boot/loader/raspberrypi/raspberrypi.nix
 
+# TODO: incorporate https://github.com/NixOS/nixos-hardware/tree/master/raspberry-pi
+
 { config, lib, pkgs, inputs, ... }:
 with builtins; with lib; {
   options.nzbr.boot.raspberrypi = with types; {
     enable = mkEnableOption "raspberrypi bootloader/firmware builder";
+    initrd = mkEnableOption "boot using an initial ramdisk";
     config = mkOption {
       type = lazyAttrsOf (
         lazyAttrsOf (
@@ -35,8 +38,12 @@ with builtins; with lib; {
         ''
           cp -r ${pkgs.raspberrypifw}/share/raspberrypi/boot/. "$PWD"
           cp ${configTxt} "$PWD"/config.txt
-          cp ${cmdlineTxt} "$PWD"/cmdline.txt
+          echo " init=${default}/init" | cat ${cmdlineTxt} - > "$PWD"/cmdline.txt
           cp ${default}/kernel "$PWD"/nixos-kernel.img
+          ${optionalString cfg.initrd ''
+            cp ${default}/initrd "$PWD"/nixos-initrd.img
+            ${pkgs.gnused}/bin/sed -i 's|^initramfs=|initramfs |' "$PWD"/config.txt
+          ''}
 
           ${cfg.extraFirmwareCommands}
         '';
@@ -45,18 +52,29 @@ with builtins; with lib; {
 
       nzbr = {
         boot = {
-          disableInitrd = true;
+          disableInitrd = !cfg.initrd;
 
           raspberrypi.config = {
             all = {
               arm_64bit = mkIf (config.nzbr.system == "aarch64-linux") 1;
               kernel = "nixos-kernel.img";
               disable_overscan = 1;
+              # disable_fw_kms_setup = 1;
               camera_auto_detect = 1;
               display_auto_detect = 1;
+              max_framebuffers = 2;
               dtoverlay = [
                 "vc4-kms-v3d"
               ];
+              dtparam = [
+                "audio=on"
+              ];
+            } // (optionalAttrs cfg.initrd {
+              initramfs = "nixos-initrd.img followkernel";
+            });
+
+            cm4 = {
+              otg_mode = 1;
             };
 
             pi4 = {
@@ -69,10 +87,6 @@ with builtins; with lib; {
           enable = true;
           firmwareSize = 512;
           populateFirmwareCommands = firmwareCommands config.system.build.toplevel;
-          populateRootCommands = ''
-            mkdir -p ./files/sbin
-            ln -s ${config.system.build.toplevel}/init ./files/sbin/init
-          '';
         };
       };
 
@@ -90,49 +104,38 @@ with builtins; with lib; {
           "root=PARTUUID=${removePrefix "0x" config.nzbr.installer.sdcard.firmwarePartitionID}-02"
           "rootfstype=ext4"
           "rootwait"
+          "net.ifnames=0"
           "plymouth.ignore-serial-consoles"
         ];
-        kernelPackages = mkDefault pkgs.linuxKernel.rpiPackages.linux_rpi4;
-        kernelPatches = [
-          kernelPatches.tuntap
-          kernelPatches.logo
-        ];
+        # kernelPackages = mkDefault pkgs.linuxKernel.rpiPackages.linux_rpi4;
+        # kernelPatches = [
+        #   kernelPatches.tuntap
+        #   kernelPatches.logo
+        # ];
       };
 
       system.boot.loader.id = "raspberrypi-nzbr";
       system.boot.loader.kernelFile = pkgs.stdenv.hostPlatform.linux-kernel.target;
-      system.build.installBootLoader =
-        let
-          initScriptBuilder = pkgs.substituteAll {
-            name = "init-script-builder.sh";
-            src = "${inputs.nixpkgs}/nixos/modules/system/boot/loader/init-script/init-script-builder.sh";
-            isExecutable = true;
-            inherit (pkgs) bash;
-            path = [ pkgs.coreutils pkgs.gnused pkgs.gnugrep ];
-          };
-        in
-        pkgs.writeShellScript "install-boot-loader.sh" ''
-          echo Creating /sbin/init
-          ${initScriptBuilder} "$1"
+      system.build.installBootLoader = pkgs.writeShellScript "install-boot-loader.sh" ''
+        echo Building firmware
+        cd $(mktemp -d)
+        ${firmwareCommands "$1"}
 
-          echo Building firmware
-          cd $(mktemp -d)
-          ${firmwareCommands "$1"}
-
-          echo Updating firmware
-          if ! mount | grep -q /boot/firmware; then
-            mount /boot/firmware
-          fi
-          mount -o remount,rw /boot/firmware
-          rsync --info=progress2 -lr --delete "$PWD"/ /boot/firmware
-          mount -o remount,ro /boot/firmware
-        '';
+        echo Updating firmware
+        if ! mount | grep -q /boot/firmware; then
+          mount /boot/firmware
+        fi
+        mount -o remount,rw /boot/firmware
+        rsync --info=progress2 -lr --delete "$PWD"/ /boot/firmware
+        mount -o remount,ro /boot/firmware
+      '';
 
       nix.gc.automatic = false; # Reduce writes to the sd card
       networking.interfaces.eth0.useDHCP = mkDefault true;
 
       environment.systemPackages = with pkgs; [
         libraspberrypi
+        raspberrypi-eeprom
       ];
 
     };
