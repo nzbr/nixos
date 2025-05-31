de8f81dc49f07585fdbd385cc209dfdb0c45ebed
nzbr <mail@nzbr.de>
2025-05-31 20:16:03 +0200

    sudo: enable pwfeedback

--- module/cli/sudo.nix
+++ module/cli/sudo.nix
@@ -12,6 +12,7 @@ with builtins; with lib; {
     ];
 
     security.sudo.extraConfig = ''
+      Defaults pwfeedback
       Defaults insults
       Defaults passprompt="[48;5;9m[38;5;15m 🔒 [38;5;9m[48;5;208m [38;5;15m[1m %p [22m[049m[38;5;208m[039m  "
       Defaults env_keep+=SSH_CONNECTION
