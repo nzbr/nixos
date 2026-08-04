80af31bb37ed46f24856aaa39d9f3db9716dc8e5
nzbr <mail@nzbr.de>
2026-08-04 23:32:42 +0200

    borgbackup: migrate to commands and fix zvol symlink race condition

--- module/service/borgbackup.nix
+++ module/service/borgbackup.nix
@@ -184,91 +184,105 @@ with builtins; with lib; {
               }
             ];
 
-            before_backup = flatten [
-              (pkgs.writeShellScript "borg-pre-backup_snapshot" ''
-                set -euxo pipefail
+            commands = [
+              {
+                before = "action";
+                when = ["create"];
+                run = [
+                  (pkgs.writeShellScript "borg-pre-backup_snapshot" ''
+                    set -euxo pipefail
 
-                umount -R ${snapshotMountPath} || true
-                mount --mkdir -t tmpfs tmpfs ${snapshotMountPath}
+                    umount -R ${snapshotMountPath} || true
+                    mount --mkdir -t tmpfs tmpfs ${snapshotMountPath}
 
-                ${concatStringsSep "\n" (
-                  map
-                  (pool:
-                    assert pool.recursive -> pool.subvols == [ ];
-                    let
-                      mountpoint =
-                        if pool.mountpoint == null
-                        then "${snapshotMountPath}/"
-                        else "${snapshotMountPath}/${removePrefix "/" pool.mountpoint}";
-                    in
-                    ''
-                      zfs destroy -r ${pool.name}@${cfg.zfs.snapshotName} || true
-                      zfs snapshot -r ${pool.name}@${cfg.zfs.snapshotName}
-                      mkdir -p ${mountpoint}
-                      ${optionalString (pool.mountpoint != null) ''
-                        mount -t zfs ${pool.name}@${cfg.zfs.snapshotName} ${mountpoint}
-                      ''}
-                      ${concatStringsSep "\n" (map
-                        (subvol: "mount --mkdir -t zfs ${pool.name}/${subvol.name}@${cfg.zfs.snapshotName} ${mountpoint}/${removePrefix "/" subvol.mountpoint}")
-                        pool.subvols
-                      )}
-                      ${optionalString pool.recursive ''
-                        mountRoot="$(zfs list -Ho mountpoint ${pool.name})"
-                        for subvol in $(zfs list -rHo name,mountpoint ${pool.name} | sed 's|${pool.name}/||' | awk 'NR!=1&&$2!="-"{print $1;}'); do
-                          subMount="$(zfs list -Ho mountpoint ${pool.name}/''${subvol})"
-                          if [[ "$subMount" != "none" && "$subMount" != "-" ]]; then
-                            if [[ "$mountRoot" == "legacy" ]]; then
-                              mount --mkdir -t zfs "${pool.name}/''${subvol}@${cfg.zfs.snapshotName}" "${mountpoint}/$subvol"
-                            else
-                              mount --mkdir -t zfs "${pool.name}/''${subvol}@${cfg.zfs.snapshotName}" "${mountpoint}''${subMount#$mountRoot}"
-                            fi
+                    ${concatStringsSep "\n" (
+                      map
+                      (pool:
+                        assert pool.recursive -> pool.subvols == [ ];
+                        let
+                          mountpoint =
+                            if pool.mountpoint == null
+                            then "${snapshotMountPath}/"
+                            else "${snapshotMountPath}/${removePrefix "/" pool.mountpoint}";
+                        in
+                        ''
+                          zfs destroy -r ${pool.name}@${cfg.zfs.snapshotName} || true
+                          zfs snapshot -r ${pool.name}@${cfg.zfs.snapshotName}
+                          ${pkgs.parted}/bin/partprobe
+                          mkdir -p ${mountpoint}
+                          ${optionalString (pool.mountpoint != null) ''
+                            mount -t zfs ${pool.name}@${cfg.zfs.snapshotName} ${mountpoint}
+                          ''}
+                          ${concatStringsSep "\n" (map
+                            (subvol: "mount --mkdir -t zfs ${pool.name}/${subvol.name}@${cfg.zfs.snapshotName} ${mountpoint}/${removePrefix "/" subvol.mountpoint}")
+                            pool.subvols
+                          )}
+                          ${optionalString pool.recursive ''
+                            mountRoot="$(zfs list -Ho mountpoint ${pool.name})"
+                            for subvol in $(zfs list -rHo name,mountpoint ${pool.name} | sed 's|${pool.name}/||' | awk 'NR!=1&&$2!="-"{print $1;}'); do
+                              subMount="$(zfs list -Ho mountpoint ${pool.name}/''${subvol})"
+                              if [[ "$subMount" != "none" && "$subMount" != "-" ]]; then
+                                if [[ "$mountRoot" == "legacy" ]]; then
+                                  mount --mkdir -t zfs "${pool.name}/''${subvol}@${cfg.zfs.snapshotName}" "${mountpoint}/$subvol"
+                                else
+                                  mount --mkdir -t zfs "${pool.name}/''${subvol}@${cfg.zfs.snapshotName}" "${mountpoint}''${subMount#$mountRoot}"
+                                fi
+                              fi
+                            done
+                          ''}
+                        ''
+                      )
+                      cfg.zfs.pools
+                    )}
+
+                    ${concatStringsSep "\n" (
+                      map
+                        (dir:
+                          let
+                            snapshot = "${dir}/${cfg.cephfs.snapDir}/${cfg.cephfs.snapshotName}";
+                          in
+                          ''
+                          if [[ -d "${snapshot}" ]]; then
+                            rmdir "${snapshot}"
                           fi
-                        done
-                      ''}
-                    ''
-                  )
-                  cfg.zfs.pools
-                )}
+                          mkdir "${snapshot}"
 
-                ${concatStringsSep "\n" (
-                  map
-                    (dir:
-                      let
-                        snapshot = "${dir}/${cfg.cephfs.snapDir}/${cfg.cephfs.snapshotName}";
-                      in
-                      ''
-                      if [[ -d "${snapshot}" ]]; then
-                        rmdir "${snapshot}"
-                      fi
-                      mkdir "${snapshot}"
+                          mount --mkdir -o bind "${snapshot}" "${snapshotMountPath}${dir}"
+                        '')
+                        cfg.cephfs.directories
+                    )}
 
-                      mount --mkdir -o bind "${snapshot}" "${snapshotMountPath}${dir}"
-                    '')
-                    cfg.cephfs.directories
-                )}
-              '')
-              (optional cfg.backupBlockDevices (pkgs.writeShellScript "borg-pre-backup_find-special" ''
-                set -euxo pipefail
-                find ${snapshotMountPath} -xtype b,c,p,s -fprint ${specialFilesList}
-                echo "Excluding $(wc -l ${specialFilesList} | ${pkgs.gawk}/bin/awk '{print $1;}') special files"
-              ''))
+                    ${optionalString cfg.backupBlockDevices (pkgs.writeShellScript "borg-pre-backup_find-special" ''
+                      set -euxo pipefail
+                      find ${snapshotMountPath} -xtype b,c,p,s -fprint ${specialFilesList}
+                      echo "Excluding $(wc -l ${specialFilesList} | ${pkgs.gawk}/bin/awk '{print $1;}') special files"
+                    '')}
+                  '')
+                ];
+              }
+
+              {
+                after = "everything";
+                when = ["create"];
+                run = [
+                  (pkgs.writeShellScript "borg-post-backup" ''
+                    set -euxo pipefail
+                    umount -R ${snapshotMountPath}
+
+                    ${concatStringsSep "\n" (map
+                      (dir: "rmdir '${dir}/${cfg.cephfs.snapDir}/${cfg.cephfs.snapshotName}'")
+                      (reverseList cfg.cephfs.directories)
+                    )}
+
+                    ${concatStringsSep "\n" (map
+                      (pool: "zfs destroy -r ${pool.name}@${cfg.zfs.snapshotName}")
+                      (reverseList cfg.zfs.pools)
+                    )}
+                  '')
+                ];
+              }
             ];
-            after_backup = [
-              (pkgs.writeShellScript "borg-post-backup" ''
-                set -euxo pipefail
-                umount -R ${snapshotMountPath}
 
-                ${concatStringsSep "\n" (map
-                  (dir: "rmdir '${dir}/${cfg.cephfs.snapDir}/${cfg.cephfs.snapshotName}'")
-                  (reverseList cfg.cephfs.directories)
-                )}
-
-                ${concatStringsSep "\n" (map
-                  (pool: "zfs destroy -r ${pool.name}@${cfg.zfs.snapshotName}")
-                  (reverseList cfg.zfs.pools)
-                )}
-              '')
-            ];
             healthchecks.ping_url = cfg.healthcheckUrl;
           }
           cfg.extraConfig
